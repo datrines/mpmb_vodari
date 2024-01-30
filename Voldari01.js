8bbd68f880bc0b3363874a8b23a4ec77f1bc97af
datrines <datrines@gmail.com>
2024-01-30 23:34:58 +0100

    Fixed bugs and added bard of nature

--- Voldari01.js
+++ Voldari01.js
@@ -19,7 +19,7 @@
 var iFileName = "Voldari01.js";
 RequiredSheetVersion(13);
 
-SourceList["Voldari"] = {
+SourceList["SOV"] = {
 	name : "Seas of Voldari",
 	abbreviation : "SOV",
 	group : "Tribality",
@@ -34,7 +34,7 @@ AddSubClass("barbarian","buccaneer",{
 	features : {
 		"subclassfeature3" : {
 			name : "Board",
-			source : [["SOV",128]],
+			source : [["SOV",126]],
 			minlevel : 3,
 			description : desc(["My profiency bonus is doubled for Strength (Athletics) ability checks made to swim",
 			                    "Further, I have a climbing and swimming speed equal to my walking speed, and have advantage on Dexterity (Stealth) ability checks made while climbing and swimming"
@@ -42,7 +42,7 @@ AddSubClass("barbarian","buccaneer",{
 		},
 		"subclassfeature3.1" : {
 			name : "Invade",
-			source : [["SOV",128]],
+			source : [["SOV",126]],
 			minlevel : 3,
 			description : desc(["I can choose to shove an enemy as a reaction when I enter a rage",
 								"While raging I can choose to shove an enemy as a bonus action",
@@ -54,7 +54,7 @@ AddSubClass("barbarian","buccaneer",{
 		},
 		"subclassfeature6" : {
 			name : "Pillage",
-			source : [["SOV",128]],
+			source : [["SOV",126]],
 			minlevel : 6,
 			description : desc(["I can dash as a reaction when entering rage",
 								"While raging I can dash as a bonus action",
@@ -66,7 +66,7 @@ AddSubClass("barbarian","buccaneer",{
 		},
 		"subclassfeature10" : {
 			name : "Capture",
-			source : [["SOV",128]],
+			source : [["SOV",126]],
 			minlevel : 10,
 			description : desc(["Advantage on all grappling checks while holding at least 6 ft of rope in one hand",
 								"Additionally, as long as I have at least 6 ft of rope in one hand, creatures I am grappling are blinded and don't automatically succeed on escaping my grapple due to their size.",
@@ -75,7 +75,7 @@ AddSubClass("barbarian","buccaneer",{
 		},
 		"subclassfeature14" : {
 			name : "Plunder",
-			source : [["SOV",128]],
+			source : [["SOV",126]],
 			minlevel : 14,
 			description : desc(["Any time a creature within 5ft of me gains disadvantage on an attack roll while I am raging, I can make an attack with advantage against that creature as a reacion",
 								"I can do so even if I have already used my reaction this turn. Can be used number of times equal to 1 + Str mod, but never more than once per turn",
@@ -85,6 +85,162 @@ AddSubClass("barbarian","buccaneer",{
 			usages : "1+Str mod per ",
 			usagescalc : "event.value = 1 + What('Str Mod');",
 			action : [["reaction", "attack (see plunder)"]]
+		}
+	}
+});
+
+AddSubClass("bard","college of nature",{
+	regExpSearch : /^(?=.*(college|bard|minstrel|troubadour|jongleur))(?=.*nature).*$/i,
+	subname : "College of Nature",
+	source : [["SOV",128]],
+	features : {
+		"subclassfeature3.1" : {
+			name : "Bonus Proficiencies",
+			source : [["SOV", 126]],
+			minlevel : 3,
+			description : desc(["When you join the College of the Nature at 3rd level, you gain proficiency in two skills of your choice selected from Animal Handling, Nature, and Survival.",
+								"In addition, you gain proficiency with the herbalism kit."]),
+			skillstxt : "Two from Animal Handling, Nature and Survival",
+			toolsProfs : [["Herbalism kit", 1]]
+		},
+		"subclassfeature3.2" : {
+			name : "Natural Focus cantrip",
+			source : [["SOV", 126]],
+			minlevel : 3,
+			description : desc(["you learn one druid cantrip of your choice. This counts as a bard cantrip for you"]),
+			spellcastingBonus : {
+				name : "Natural focus druid cantrip",
+				"class" : "druid",
+				level : [0, 0]
+			}
+		},
+		"subclassfeature3.3" : {
+			name : "Natural Focus environment",
+			source : [["SOV", 126]],
+			minlevel : 3,
+			description : desc(["You gain a natural focus, a specific environment that you feel a connection with.  Use 'Choose Feature' button"]),
+			choices : ["Arctic", "Coast", "Desert", "Forest", "Grassland", "Mountain", "Swamp"],
+			"arctic" : {
+				name : "Arctic natural focus environment",
+				description	: desc(["Frozen wastelands are a second home to you. You have resistance to cold damage, and slick ice is not difficult terrain for you."]),
+				dmgres : ["Cold"]
+			},
+            "coast" : {
+				name : "Coast natural focus environment",
+				description	: desc(["You are as comfortable on land as in the water. You have a swimming speed of 30 feet, and you can breathe air and water."]),
+				speed : { swim : {spd: 30, enc: 20} }
+			},
+			"desert" : {
+				name : "Desert natural focus environment",
+				description	: desc(["The blazing sun and blistering heat of the desert is a comfort. You have resistance to fire damage and you require only half a gallon of water per day."]),
+				dmgres : ["Fire"]
+			},
+			"forest" : {
+				name : "Forest natural focus environment",
+				description	: desc(["You feel at home high in the tallest trees. You have a climbing speed of 30 feet. Also, you have advantage on Dexterity (Stealth) checks while in a forest environment. "]),
+				speed : { climb : {spd: 30, enc:20}}
+			},
+			"grassland" : {
+				name : "Grassland natural focus environment",
+				description	: desc(["The vast grasslands have taught you to travel quickly and quietly. Your speed increases by 5 feet while you are not wearing heavy armor. Also, you have advantage on Dexterity (Stealth) checks while in a grassland environment."])
+			},
+			"mountain" : {
+				name : "Mountain natural focus environment",
+				description	: desc(["The storming skies high above the mountains bring you calm and peace. You have resistance to lightning damage. Also, you’re acclimated to high altitude, including elevations above 20,000 feet."]),
+				dmgres : ["Lightning"]
+			},
+			"swamp" : {
+				name : "Swamp natural focus environment",
+				description	: desc(["You enjoy spending time in thick, foggy swamps. You have resistance to poison damage and can ignore difficult terrain while in a swamp environment."]),
+				dmgres : ["Poison"]
+			},
+		},
+		"subclassfeature6.1" : {
+			name : "Environmental Magical Secrets druid/ranger spells",
+			description : desc(["whenever you learn a new bard spell, you may instead choose a spell from the druid or ranger spell list. A spell you choose must be of a level you can cast, as shown on the Bard table, and the spell must be of 1st level or higher."]),
+			source : [["SOV", 127]],
+			minlevel : 6,
+			spellcastingList : {
+				// TODO fix this shit, doesn't work for some reason
+				"class" : ["bard", "druid", "ranger"]
+			}
+		},
+		"subclassfeature6.2" : {
+			name : "Environmental Magical Secrets spells",
+			description : desc(["Additional spells gained from the natural focus feature at level 3 (Choose again in menu!)"]),
+			//TODO make it dependent on L3 choice automatically
+			source : [["SOV", 127]],
+			minlevel : 6,
+			choices : ["Arctic", "Coast", "Desert", "Forest", "Grassland", "Mountain", "Swamp"],
+			"arctic" : {
+				name : "Environmental Magical Secrets: arctic",
+				spellcastingBonus : {
+					name : "Environmental Magical Secrets: arctic",
+					spells : ["hold person", "sleet storm"],
+					selection : ["hold person", "sleet storm"],
+					times : 2
+				}
+			},
+			"coast" : {
+				name : "Environmental Magical Secrets: coast",
+				spellcastingBonus : {
+					name : "Environmental Magical Secrets: coast",
+					spells : ["misty step", "call lightning"],
+					selection : ["misty step", "call lightning"],
+					times : 2
+				}
+			},
+			"desert" : {
+				name : "Environmental Magical Secrets: desert",
+				spellcastingBonus : {
+					name : "Environmental Magical Secrets: desert",
+					spells : ["scorching ray", "wind wall"],
+					selection : ["scorching ray", "wind wall"],
+					times : 2 
+				}
+			},
+			"forest" : {
+				name : "Environmental Magical Secrets: forest",
+				spellcastingBonus : {
+					name : "Environmental Magical Secrets: forest",
+					spells : ["barkskin", "conjure animals"],
+					selection : ["barkskin", "conjure animals"],
+					times : 2 
+				}
+			},
+			"grassland" : {
+				name : "Environmental Magical Secrets: grassland",
+				spellcastingBonus : {
+					name : "Environmental Magical Secrets: grassland",
+					spells : ["pass without a trace", "wind wall"],
+					selection : ["pass without a trace", "wind wall"],
+					times : 2
+				}
+			},
+			"mountain" : {
+				name : "Environmental Magical Secrets: mountain",
+				spellcastingBonus : {
+					name : "Environmental Magical Secrets: mountain",
+					spells : ["spider climb", "meld into stone"],
+					selection : ["spider climb", "meld into stone"],
+					times : 2
+				}
+			},
+			"swamp" : {
+				name : "Environmental Magical Secrets: swamp",
+				spellcastingBonus : {
+					name : "Environmental Magical Secrets: swamp",
+					spells : ["spider climb", "meld into stone"],
+					selection : ["spider climb", "meld into stone"],
+					times : 2
+				}
+			}
+		},
+		"subclassfeature14" : {
+			name : "Natural inspiration",
+			description : desc(["A creature that uses a Bardic Inspiration die from you when rolling an ability check, attack roll, or saving throw ignores any disadvantage to that roll, in addition to adding the Bardic Inspiration die to the result."]),
+			source : [["SOV", 127]],
+			minlevel : 14,
 		}
 	}
 });
